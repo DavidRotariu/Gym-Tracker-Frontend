f6e21851776ee1a0c357084f1dee2094c3c2b1e2
David Rotariu <davidrotariu139@gmail.com>
2025-03-16 20:27:17 +0200

    fix: login link

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,10 +4,3 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-import { createClient } from "@supabase/supabase-js";
-
-const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
